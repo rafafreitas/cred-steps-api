ab972261e165afce45d0de55c819b0a904222a5f
Rafael Freitas <rafael.vasconcelos@outlook.com>
2018-08-01 21:40:00 -0300

    Run| Criação e chamada do header

---
+++ Header/index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rafael Freitas
+ * Date: 01/08/2018
+ * Time: 19:13
+ */
+
+date_default_timezone_set('America/Recife');
+header("Content-Type: application/json");
+require 'lib/vendor/autoload.php';
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$app = new \Slim\App(array('templates.path' => 'templates', 'settings' => ['displayErrorDetails' => true]));
+
+$app->options('/{routes:.+}', function ($request, $response, $args){
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+
+$app->get('/', function(Request $request, Response $response, $args) {
+    return $response->withJson(['status' => 200, 'message' => "Api Manager Cred-Steps"]);
+});
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rafael Freitas
+ * Date: 01/08/2018
+ * Time: 19:13
+ */
+
+require 'Header/index.php';
+
+require 'Groups/UsuarioGroup.php';
+
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+    $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+    return $handler($req, $res);
+});
+
+$app->run();
