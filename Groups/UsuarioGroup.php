cf9c56792165000959535d15cd4598158587f203
Rafael Freitas <rafael.vasconcelos@outlook.com>
2018-08-01 21:42:11 -0300

    User | Ajustes no grupo de usuários.

--- Groups/UsuarioGroup.php
+++ Groups/UsuarioGroup.php
@@ -6,21 +6,24 @@
  * Time: 18:56
  */
 
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
 require_once 'Controller/Authorization.php';
 require_once 'Controller/UserController.php';
 
-$app->group('user', function (){
+$app->group('', function (){
 
     //List-All
-    $this->get('/', function ($request, $response, $args) {
+    $this->get('/users', function ($request, $response, $args) {
 
-        $auth = new Authorization();
-        $check = $auth->verificarToken($request);
-
-        if ($check['status'] != 200) {
-            return $response->withJson($auth, $auth['status']);
-            die;
-        }
+//        $auth = new Authorization();
+//        $check = $auth->verificarToken($request);
+//
+//        if ($check['status'] != 200) {
+//            return $response->withJson($check, $check['status']);
+//            die;
+//        }
 
         $userController = new UserController();
         $retorno = $userController->getUsers();
