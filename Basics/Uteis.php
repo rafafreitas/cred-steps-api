18783274ce23610c299b1070e1802db2f1e6e37b
Rafael Freitas <rafael.vasconcelos@outlook.com>
2018-08-07 08:00:47 -0300

    SendEmail | Iniciado implementação do select para os clientes.
    Criação de máscara.

--- Basics/Uteis.php
+++ Basics/Uteis.php
@@ -62,4 +62,21 @@ class Uteis
         file_put_contents($file, $image_base64);
         return $output_file;
     }
+
+    function mask($val, $mask){
+        $maskared = '';
+        $k = 0;
+        for($i = 0; $i<=strlen($mask)-1; $i++) {
+            if($mask[$i] == '#') {
+                if(isset($val[$k])) {
+                    $maskared .= $val[$k++];
+                }
+            } else{
+                if(isset($mask[$i])) {
+                    $maskared .= $mask[$i];
+                }
+            }
+        }
+        return $maskared;
+    }
 }
